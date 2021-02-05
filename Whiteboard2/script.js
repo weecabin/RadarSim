df1a44162cd218aa4e7d0ea25ad4b599b9698116
play.js <git@gituser.com>
2021-02-04 19:06:16 -0800

    Initial Commit

---
+++ Whiteboard2/script.js
@@ -0,0 +1,346 @@
+
+
+        // get our canvas element
+        const canvas = document.getElementById("canvas");
+        const context = canvas.getContext("2d");
+
+        // disable right clicking
+        document.oncontextmenu = function () {
+            return false;
+        }
+
+        // list of all strokes drawn
+        const drawings = [];
+
+        var vt = new ViewTools(canvas);
+
+        function redrawCanvas() {
+            // set the canvas to the size of the window
+            canvas.width = 400;
+            canvas.height = 400;
+            //canvas.width = document.body.clientWidth;
+            //canvas.height = document.body.clientHeight;
+
+            context.fillStyle = '#fff';
+            context.fillRect(0, 0, canvas.width, canvas.height);
+            for (let i = 0; i < drawings.length; i++) 
+            {
+                const line = drawings[i];
+                drawLine(vt.toScreenX(line.x0), vt.toScreenY(line.y0),
+                         vt.toScreenX(line.x1), vt.toScreenY(line.y1));
+            }
+            if (Objs!=undefined)
+            {
+              for (let mv of Objs)
+              {
+                mv.Draw(context);
+              }
+            }
+        }
+        redrawCanvas();
+
+        // if the window changes size, redraw the canvas
+        window.addEventListener("resize", (event) => {
+            redrawCanvas();
+        });
+
+        // Touch Event Handlers 
+        canvas.addEventListener('touchstart', onTouchStart);
+        canvas.addEventListener('touchend', onTouchEnd);
+        canvas.addEventListener('touchcancel', onTouchEnd);
+        canvas.addEventListener('touchmove', onTouchMove);
+
+        function drawLine(x0, y0, x1, y1) {
+            context.beginPath();
+            context.moveTo(x0, y0);
+            context.lineTo(x1, y1);
+            context.strokeStyle = '#000';
+            context.lineWidth = 2;
+            context.stroke();
+        }
+
+        // touch functions
+        const prevTouches = [null, null]; // up to 2 touches
+        let singleTouch = false;
+        let doubleTouch = false;
+        var dragmv = undefined; // dragging from this moving vector object 
+        var dragto = undefined;
+        function onTouchStart(event) {
+            AddStatus("in Touch Start");
+            if (event.touches.length == 1) {
+                singleTouch = true;
+                doubleTouch = false;
+            }
+            if (event.touches.length >= 2) {
+                singleTouch = false;
+                doubleTouch = true;
+            }
+
+            // store the last touches
+            prevTouches[0] = event.touches[0];
+            prevTouches[1] = event.touches[1];
+
+        }
+        function onTouchMove(event) {
+            // get first touch coordinates
+            const touch0X = event.touches[0].pageX;
+            const touch0Y = event.touches[0].pageY;
+            const prevTouch0X = prevTouches[0].pageX;
+            const prevTouch0Y = prevTouches[0].pageY;
+
+            const scaledX = vt.toTrueX(touch0X);
+            const scaledY = vt.toTrueY(touch0Y);
+            const prevScaledX = vt.toTrueX(prevTouch0X);
+            const prevScaledY = vt.toTrueY(prevTouch0Y);
+
+            if (singleTouch) 
+            {
+              if (get("sketch").checked)
+              {
+                // add to history
+                drawings.push({
+                    x0: prevScaledX,
+                    y0: prevScaledY,
+                    x1: scaledX,
+                    y1: scaledY
+                })
+                drawLine(prevTouch0X, prevTouch0Y, touch0X, touch0Y);
+              }
+              else
+              {
+                if (dragmv==undefined)
+                {
+                  //AddStatus("Find the closest plane");
+                  let tempmv=new MovingVector(1,1,scaledX,scaledY);
+                  let closestmv=Objs[0];
+                  //AddStatus("checking distance with temp");
+                  let dist=DistBetween(tempmv,closestmv);
+                  for(let mv of Objs)
+                  {
+                    //AddStatus("checking distance");
+                    let testdist=DistBetween(mv,tempmv);
+                    if (testdist<dist)
+                    {
+                      //AddStatus("found closest="+mv.xpos+","+mv.ypos);
+                      dist=testdist;
+                      closestmv=mv;
+                    }
+                  }
+                  dragmv=closestmv;
+                }
+                dragto=[touch0X,touch0Y]
+              }
+            }
+
+            if (doubleTouch) {
+                // get second touch coordinates
+                const touch1X = event.touches[1].pageX;
+                const touch1Y = event.touches[1].pageY;
+                const prevTouch1X = prevTouches[1].pageX;
+                const prevTouch1Y = prevTouches[1].pageY;
+
+                // get midpoints
+                const midX = (touch0X + touch1X) / 2;
+                const midY = (touch0Y + touch1Y) / 2;
+                const prevMidX = (prevTouch0X + prevTouch1X) / 2;
+                const prevMidY = (prevTouch0Y + prevTouch1Y) / 2;
+
+                // calculate the distances between the touches
+                const hypot = Math.sqrt(Math.pow((touch0X - touch1X), 2) + Math.pow((touch0Y - touch1Y), 2));
+                const prevHypot = Math.sqrt(Math.pow((prevTouch0X - prevTouch1X), 2) + Math.pow((prevTouch0Y - prevTouch1Y), 2));
+
+                // calculate the screen scale change
+                var zoomAmount = hypot / prevHypot;
+                vt.scale = vt.scale * zoomAmount;
+                const scaleAmount = 1 - zoomAmount;
+
+                // calculate how many pixels the midpoints have moved in the x and y direction
+                const panX = midX - prevMidX;
+                const panY = midY - prevMidY;
+                // scale this movement based on the zoom level
+                vt.offsetX += (panX / vt.scale);
+                vt.offsetY += (panY / vt.scale);
+
+                // Get the relative position of the middle of the zoom.
+                // 0, 0 would be top left. 
+                // 0, 1 would be top right etc.
+                var zoomRatioX = midX / canvas.clientWidth;
+                var zoomRatioY = midY / canvas.clientHeight;
+
+                // calculate the amounts zoomed from each edge of the screen
+                const unitsZoomedX = vt.trueWidth() * scaleAmount;
+                const unitsZoomedY = vt.trueHeight() * scaleAmount;
+
+                const unitsAddLeft = unitsZoomedX * zoomRatioX;
+                const unitsAddTop = unitsZoomedY * zoomRatioY;
+
+                vt.offsetX += unitsAddLeft;
+                vt.offsetY += unitsAddTop;
+
+                redrawCanvas();
+            }
+            prevTouches[0] = event.touches[0];
+            prevTouches[1] = event.touches[1];
+        }
+        function onTouchEnd(event) 
+        {
+          singleTouch = false;
+          doubleTouch = false;
+
+          if (Objs.length==0 || dragmv==undefined)return;
+          //AddStatus(dragmv.Snapshot());
+          let dragVector=new Vector(vt.toTrueX(dragto[0])-dragmv.xpos,
+                                    vt.toTrueY(dragto[1])-dragmv.ypos);
+          // allow the user to cancel the vector by moving back to the origin
+          if (dragVector.GetLength()<25)
+          {
+            dragmv=undefined;
+            return;
+          }
+          dragmv.vector.SetDirection(dragVector.GetDirection());
+          dragmv=undefined;
+        }
+
+function get(id)
+{
+  return document.getElementById(id);
+}
+
+
+var Objs=[];
+function AddPlane()
+{
+  AddStatus("In AddPlane");
+  let speed=1;
+  let plane={type:"plane",length:12,width:6,color:"black",
+               drag:0,gravity:0};
+  let movingVector = new MovingVector(.1,.1,0,0,plane,vt);
+  //AddStatus(JSON.stringify(movingVector));
+  Objs.push(movingVector);
+}
+
+function StartAnimation()
+{
+  if (runAnimate)
+  {
+    runAnimate=false;
+    get("animate").value="Start Animation";
+  }
+  else
+  {
+    runAnimate=true;
+    get("animate").value="Stop Animation";
+    Animate();
+  }
+}
+
+let runAnimate=false;
+function Animate()
+{
+try
+  {
+  AddStatus("In Animate");
+  var id = setInterval(frame, 30);
+  function frame() 
+  {
+    try
+    {
+    //AddStatus("in Frame");
+    if (!runAnimate) 
+    {
+      clearInterval(id);
+      Objs=[];
+    } 
+    else 
+    {
+      // world edge is canvas size / zoom
+      for (let mv of Objs)
+      {
+        mv.Move();
+      } 
+      //AddStatus("Look for collisions with other objects");
+      for (let i=0;get("collision").checked &&  i<Objs.length-1;i++)
+      {
+        for (let j=i+1;j<Objs.length;j++)
+        {
+          if (DistBetween(Objs[i],Objs[j])<10)
+          {
+            let v1=Objs[i].vector;
+            let v2=Objs[j].vector;
+            let movingAway=Objs[i].MovingAway(Objs[j]);
+            if (!movingAway)
+            {
+              CollisionBounce(Objs[i],Objs[j])
+              Objs[i].Move();
+              Objs[j].Move();
+            }
+          }
+        }
+      }
+      //AddStatus("Clear, then draw everything");
+      redrawCanvas();
+      if (dragmv!=undefined)
+      {
+        drawLine(vt.toScreenX(dragmv.xpos),vt.toScreenY(dragmv.ypos),
+                 dragto[0],dragto[1]);
+        //AddStatus(dragmv.xpos+","+dragmv.ypos);
+      }
+      get("debug01").innerHTML=Objs.length+" Objects";
+      for (let mv of Objs)
+      {
+        mv.Draw(context);
+      }
+    }
+    }
+    catch(err)
+    {
+      AddStatus("frame:"+err.message);
+    }
+  }
+  }
+  catch(err)
+  {
+    AddStatus("Animate:"+err.message);
+  }
+}
+
+function CollisionBounce(mv1,mv2)
+{
+  /*
+  In an elastic collision between spheres, bodies retain their speed in the 
+  direction tangent to the collision. they swap speeds in the direction
+  normal to the collision point.
+  */
+  // get the vector between the two center points
+  // this will be normal to the collision point
+  let normal = new Vector(mv2.xpos-mv1.xpos,mv2.ypos-mv1.ypos);
+  // get the tangent vector
+  let tangent = normal.UnitNormal();
+  //AddStatus("normal\n"+JSON.stringify(normal));
+  //AddStatus("tangent\n"+JSON.stringify(tangent));
+  //setup mv2
+  let projTangent2=mv2.vector.ProjectOn(tangent);
+  let otherNormal2=mv1.vector.ProjectOn(normal);
+  let newVector2=projTangent2.Add(otherNormal2);
+
+  //AddStatus("normal dir = "+normal.GetDirection());
+  //AddStatus("tangent dir = "+tangent.GetDirection());
+  //AddStatus("projTangent2 dir = "+projTangent2.GetDirection());
+  //AddStatus("otherNormal2 dir = "+otherNormal2.GetDirection());
+  //AddStatus("newVector2 dir = "+newVector2.GetDirection());
+
+  // setup mv1
+  let projTangent1=mv1.vector.ProjectOn(tangent);
+  let OtherNormal1=mv2.vector.ProjectOn(normal);
+  let newVector1=projTangent1.Add(OtherNormal1);
+
+  mv1.vector=newVector1;
+  mv2.vector=newVector2; 
+}
+
+function DistBetween(mv1,mv2)
+{
+  let dist = Math.hypot(mv2.xpos-mv1.xpos,mv2.ypos-mv1.ypos); 
+  //AddStatus("mv1/mv2=["+mv1.xpos+","+mv1.ypos+"]["+mv2.xpos+","+mv2.ypos+"]");
+  return dist;
+}
---
+++ WhoteBoardTools/script.js
@@ -0,0 +1,470 @@
+
+class ViewTools
+{
+  constructor(canvas)
+  {
+    this.canvas=canvas;
+    this.ctx=context;
+
+    // distance from origin
+    this.offsetX = 0;
+    this.offsetY = 0;
+
+    // zoom amount
+    this.scale = 1;
+  }
+  // convert coordinates
+  toScreenX(xTrue)
+  {
+    return (xTrue + this.offsetX) * this.scale;
+  }
+  toScreenY(yTrue) 
+  {
+    return (yTrue + this.offsetY) * this.scale;
+  }
+  toTrueX(xScreen) 
+  {
+    return (xScreen / this.scale) - this.offsetX;
+  }
+  toTrueY(yScreen) 
+  {
+    return (yScreen / this.scale) - this.offsetY;
+  }
+  trueHeight() 
+  {
+    return this.canvas.clientHeight / this.scale;
+  }
+  trueWidth() 
+  {
+    return this.canvas.clientWidth / this.scale;
+  }
+}
+        
+
+        
+
+
+/*************************************************************
+**************************************************************
+                         Quicky Functions
+
+Description
+
+Parameters
+
+Return Value
+
+*************************************************************/ 
+const ToDegrees = radians => (radians * 180) / Math.PI
+const ToRadians = degrees => (degrees * Math.PI) / 180
+
+const EPSILON = 0.00000001
+const AreEqual = (one, other, epsilon = EPSILON) =>
+  Math.abs(one - other) < epsilon
+/*************************************************************
+**************************************************************
+FunctionName: get
+
+Description
+returns a reference to the HTML element with an id of id
+
+Parameters
+id: the id of the element to return
+
+Return Value
+reference to the specified HTML element
+*************************************************************/ 
+function get(id)
+{
+  return document.getElementById(id);
+}
+
+/*************************************************************
+**************************************************************
+FunctionName: AddStatus
+
+Description
+Appends a string to an HTML element with an id of "status"
+Should be a textarea. Normally used to display debug info
+when debugMode is set to true. debugMode=false can be overridden
+by setting alwaysShow to true
+
+Parameters
+str: the string to add to the status element
+alwaysShow: set true to always add str independent of debugMode
+
+Return Value
+none
+*************************************************************/ 
+var debugMode=true;
+function AddStatus(str,alwaysShow=false)
+{
+  if (str==undefined)str="";
+  if (debugMode || alwaysShow)
+    get("status").value += "\n"+str;
+}
+
+/*************************************************************
+**************************************************************
+                       class Vector
+
+Description
+
+Parameters
+
+Return Value
+
+*************************************************************/ 
+
+class Vector
+{
+  constructor(x,y)
+  {
+    this.x=x;
+    this.y=y;
+    this.toRadian=Math.PI/180;
+  }
+  //functions that modify this vector
+  
+  ScaleMe(multiplier)
+  {
+    this.x*=multiplier;
+    this.y*=multiplier;
+  }
+
+  SetLength(len)
+  {
+    let mult = len/this.GetLength();
+    this.x*=mult;
+    this.y*=mult;
+  }
+  RotateMe(degrees)
+  {
+    //AddStatus("Entering RotateMe("+degrees+")");
+    let cd = this.GetDirection();
+    //AddStatus("current direction="+cd);
+    let len = this.GetLength();
+    //AddStatus("current length="+len)
+    let radiandir = (cd+degrees)*this.toRadian;
+    //AddStatus("radiandir="+radiandir);
+    this.x=len*Math.cos(radiandir);
+    this.y=len*Math.sin(radiandir);
+    //AddStatus("new x,y="+this.x+","+this.y);
+  }
+  Negate()
+  {
+    this.x*=-1;
+    this.y*=-1;
+  }
+  SetDirection(direction)
+  {
+    let dir=direction%360;
+    if (dir<0)dir+=360;
+    let len=Math.hypot(this.x,this.y);
+    this.x=len*Math.cos(dir*this.toRadian);
+    this.y=len*Math.sin(dir*this.toRadian);
+  }
+
+  //functions that return a Vector object
+  ScaleBy(scaleFactor)
+  {
+    let scaled=new Vector(this.x*scaleFactor,this.y*scaleFactor);
+    return scaled;
+  }
+  ProjectOn(other)
+  {
+    const unit = other.Unit()
+    return unit.ScaleBy(this.Dot(unit))
+  }
+  Add(v)
+  {
+    return(new Vector(this.x+v.x,this.y+v.y));
+  }
+
+  Unit()
+  {
+    return new Vector(this.x,this.y).ScaleBy(1/this.GetLength());
+  }
+
+  Rotate(degrees)
+  {
+    //AddStatus("Entering Rotate");
+    //AddStatus("Rotate("+degrees+")");
+    let retVector=new Vector(this.x,this.y);
+    //AddStatus("new Vector(this.x,this.y)="+JSON.stringify(retVector));
+    let thisdir = this.GetDirection();
+    //AddStatus("this direction="+thisdir);
+    let newdir = thisdir+degrees;
+    //AddStatus("new direction="+newdir);
+    retVector.SetDirection(newdir);
+    //AddStatus("retVector.SetDirection(newdir)="+JSON.stringify(retVector));
+    //AddStatus("Exiting Rotate");
+    return retVector;
+  }
+
+  UnitNormal()
+  {
+    let unit=this.Unit();
+    unit.SetDirection(unit.GetDirection()+90);
+    return unit;
+  }
+
+  //functions that return scalar results
+  GetDirection()
+  {
+    //AddStatus("Entering GetDirection");
+    let x=this.x;
+    if(x==0)x=.00000000001;
+    let dir=(Math.atan(this.y/x))/this.toRadian;
+    //AddStatus(dir);
+    if (x<0)dir+=180;
+    if (x>0 && this.y<0)dir+=360;
+    //AddStatus(dir);
+    return dir;
+  }
+  Dot(other)
+  {
+    return other.x * this.x + other.y * this.y;
+  }
+
+  GetLength()
+  {
+    //return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2))
+    return Math.hypot(this.x,this.y);
+  }
+
+  AngleBetween(other) 
+  {
+    //AddStatus("direction: other,this="+other.GetDirection()+","+ this.GetDirection());
+    let between=(other.GetDirection()-this.GetDirection());
+    if (between<0)between+=360;
+    if (between>180)between=between-360;
+    //AddStatus("between="+between);
+    return between;
+  }
+  
+  // Functions that return boolean
+  IsSameDirection(other) 
+  {
+    const dotProduct = this.Unit().Dot(other.Unit())
+    return AreEqual(dotProduct, 1)
+  }
+
+  IsOppositeDirection(other) 
+  {
+    const dotProduct = this.Unit().Dot(other.Unit())
+    return AreEqual(dotProduct, -1)
+  }
+
+  IsPerpendicularTo(other) 
+  {
+    const dotProduct = this.Unit().Dot(other.Unit())
+    return AreEqual(dotProduct, 0)
+  }
+
+  IsEqual(other)
+  {
+    return AreEqual(this.x,other.x) && AreEqual(this.y,other.y);
+  }
+}
+
+/*************************************************************
+**************************************************************
+                       class MovingVector
+
+Description
+
+Parameters
+
+Return Value
+
+*************************************************************/ 
+const circleObj={type:"circle",radius:15,color:"black",drag:0,gravity:0};
+const squareObj={type:"square",sidelen:15,color:"black",drag:0,gravity:0};
+const planeObj={type:"plane",length:20,width:15,color:"black",drag:0,gravity:0};
+class MovingVector
+{
+  constructor(xlen,ylen,startx,starty,drawObject=circleObj,view=null)
+  {
+    this.vector= new Vector(xlen,ylen);
+    this.xpos=startx;
+    this.ypos=starty;
+    this.drawObject=drawObject;
+    this.turnTargetDirection=this.turnDeltaAngle=0;
+    this.vt=view;
+    AddStatus(JSON.stringify(this.drawObject));
+    AddStatus("View="+JSON.stringify(this.vt));
+  }
+  Snapshot()
+  {
+    let ret="MovingVector Snapshot\n"+
+    "vector: "+JSON.stringify(this.vector)+"\n"+
+    "World x,y: "+this.xpos.toFixed(2)+","+this.ypos.toFixed(2)+"\n"+
+    "DrawObj: "+JSON.stringify(this.drawObject)+"\n"+
+    "View: "+JSON.stringify(this.view);
+    return ret;
+  }
+  SlewTo(vector)
+  {
+    //AddStatus("Entering SlewTo(vector)");
+    let angleBetween=this.vector.AngleBetween(vector);
+    let slewRate=this.vector.GetLength();
+    this.turnDeltaAngle=1.5*((angleBetween>0)?slewRate:-slewRate);
+    this.turnTargetDirection=vector.GetDirection();
+    //AddStatus("Exiting SlewTo(vector)");
+  }
+  // drawArray = [{move:"line"/"move"/"stroke",dx:1,dy:1}, ...]
+  DrawPath(ctx,drawArray,rotate=0)
+  {
+    //AddStatus("Entering DrawPath");
+    let firstMove=true
+    let zoomMult=1;
+    for (let da of drawArray)
+    {
+      let x;
+      let y;
+      //AddStatus("x,y="+x+","+y);
+      if (rotate!=0)
+      {
+        let theta = rotate*Math.PI/180;
+        let newdx = da.dx*Math.cos(theta)+da.dy*Math.sin(theta);
+        let newdy = -da.dx*Math.sin(theta)+da.dy*Math.cos(theta);
+        //AddStatus("newdx,newdy="+newdx+","+newdy);
+        x=this.xpos+newdx/zoomMult;
+        y=this.ypos+newdy/zoomMult;
+      }
+      else
+      {
+        x = this.xpos+da.dx/zoomMult;
+        y = this.ypos+da.dy/zoomMult;
+      }
+      switch (da.move)
+      {
+      case "move":
+      if (firstMove)
+        firstMove=false;
+      else
+        ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(vt.toScreenX(x),vt.toScreenY(y));
+      break;
+      case "line":
+      ctx.lineTo(vt.toScreenX(x),vt.toScreenY(y));
+      break;
+      }
+    }
+    ctx.stroke();
+    //AddStatus("Exiting DrawPath");
+  }
+
+  Draw(ctx)
+  {
+    //AddStatus("Entering Draw");
+    let drw=this.drawObject;
+    let xpos = this.xpos;
+    let ypos = this.ypos;
+    switch (drw.type)
+    {
+      case "circle":
+      //{type:"circle",radius:15,color:"black"};
+      ctx.beginPath();
+      ctx.arc(vt.toScreenX(xpos), vt.toScreenY(ypos), 
+              drw.radius*vt.scale, 0, 2 * Math.PI);
+      if (drw.color=="red")
+      {
+        ctx.fillStyle=drw.color;
+        ctx.fill();
+      }
+      ctx.stroke();
+      break;
+
+      case "plane":
+      let rotate = this.vector.GetDirection();
+      let halflen=drw.length/2;
+      let halfwid=drw.width/2;
+      var ma = 
+      [
+      {move:"move",dx:-halflen,dy:0},
+      {move:"line",dx:halflen, dy:0},
+      {move:"line",dx:0,            dy:halfwid},
+      {move:"move",dx:0,            dy:-halfwid},
+      {move:"line",dx:halflen,  dy:0},
+      ];
+      this.DrawPath(ctx,ma,-rotate);
+      break;
+    }
+    //AddStatus("Exiting Draw");
+  }
+
+  Move()
+  {
+    //AddStatus("Entering Move()");
+    try
+    {
+    if (this.drawObject.drag!=0)
+    {
+      this.vector.ScaleMe(1-this.drawObject.drag);
+    }
+    if (this.drawObject.gravity!=0)
+    { 
+      this.vector.y-=this.drawObject.gravity; 
+    }
+      
+    //AddStatus("Entering Move");
+    if (this.turnDeltaAngle!=0)
+    {
+      //AddStatus("this.turnDeltaAngle!=0");
+      let deltaAngle=this.turnTargetDirection-this.vector.GetDirection();
+      if (Math.abs(deltaAngle)<Math.abs(this.turnDeltaAngle))
+      {
+        this.vector=this.vector.ProjectOn(
+                    this.vector.Rotate(deltaAngle));
+        this.turnDeltaAngle=0;
+      }
+      else
+      {
+        let nextVector = this.vector.Rotate(this.turnDeltaAngle);
+        //AddStatus("Next Vector...\n"+JSON.stringify(nextVector));
+        this.vector=this.vector.ProjectOn(nextVector);
+      }
+    }
+    this.xpos+=this.vector.x;
+    this.ypos+=this.vector.y;
+    //AddStatus("Exiting Move");
+    }
+    catch(err)
+    {
+      AddStatus(err);
+    }
+    //AddStatus("Exiting Move()");
+  }
+
+  MovingAway(that,debug=false)
+  { 
+    let thisnextx=this.xpos+this.vector.x;
+    let thisnexty=this.ypos+this.vector.y;
+
+    let thatnextx=that.xpos+that.vector.x;
+    let thatnexty=that.ypos+that.vector.y;
+
+    let dist=Math.hypot(this.xpos-that.xpos,this.ypos-that.ypos);
+    let nextdist=Math.hypot(thisnextx-thatnextx,thisnexty-thatnexty);
+
+    if (debug)
+    {
+      AddStatus("thisx = "+this.xpos);
+      AddStatus("thisy = "+this.ypos);
+      AddStatus("thatx = "+that.xpos);
+      AddStatus("thaty = "+that.ypos);
+
+      AddStatus("thisnextx = "+thisnextx);
+      AddStatus("thisnexty = "+thisnexty);
+      AddStatus("thatnextx = "+thatnextx);
+      AddStatus("thatnexty = "+thatnexty);
+
+      AddStatus("initial dist = "+dist);
+      AddStatus("next dist = "+nextdist);
+    }
+    return nextdist>dist;
+  }
+}
